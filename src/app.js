cd2bdeb071a7c3b0f9c806070ac6f11fb9e6d1d8
David Woolf <davidjohnwoolf@gmail.com>
2018-12-09 15:21:28 -0800

    set up header

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require('../sass/base.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import About from './components/about.js'
 import Skills from './components/skills.js'
@@ -12,12 +12,25 @@ import Projects from './components/projects.js'
 
 ReactDOM.render(
 	<Router>
-		<Switch>
-			<Route exact path='/about' component={ About } />
-			<Route exact path='/skills' component={ Skills } />
-			<Route exact path='/experience' component={ Experience } />
-			<Route exact path='/projects' component={ Projects } />
-		</Switch>
+		<div>
+			<header>
+				<h1>David Woolf</h1>
+				<nav>
+					<ul>
+						<li><Link to='/about'>About</Link></li>
+						<li><Link to='/about'>Skills</Link></li>
+						<li><Link to='/about'>Experience</Link></li>
+						<li><Link to='/about'>Projects</Link></li>
+					</ul>
+				</nav>
+			</header>
+			<Switch>
+				<Route exact path='/about' component={ About } />
+				<Route exact path='/skills' component={ Skills } />
+				<Route exact path='/experience' component={ Experience } />
+				<Route exact path='/projects' component={ Projects } />
+			</Switch>
+		</div>
 	</Router>,
     document.querySelector('#app')
 );
