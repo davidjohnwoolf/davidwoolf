117946fc0f3e2f366784101171e4c2e803dceb4f
David Woolf <davidjohnwoolf@gmail.com>
2018-12-14 01:53:34 -0800

    clean up router

--- src/app.js
+++ src/app.js
@@ -1,32 +1,18 @@
 //import css for webpack
 require('../sass/base.scss');
 
-//modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//lib
 import createRouter from './lib/create-router'
 
-//containers
 import AppContainer from './containers/app.container';
 
-//components
 import Home from './components/home';
 import Projects from './components/projects';
 import Skills from './components/skills';
 import Experience from './components/experience';
 import Contact from './components/contact';
-
-const log1 = action => {
-	console.log('middleware 1 running...')
-	return action
-}
-
-const log2 = action => {
-	console.log('middleware 2 running...')
-	return action
-}
 
 const routes = [
 	{ path: '/', action: () => <Home /> },
@@ -36,13 +22,10 @@ const routes = [
 	{ path: '/contact', action: () => <Contact /> }
 ]
 
-//initialize router
-const router = createRouter({ routes, container: AppContainer, middlewares: [log1, log2] })
+const router = createRouter({ routes, container: AppContainer })
 
-const render = e => ReactDOM.render(router.resolveRoute(e), document.querySelector('#app'));
+const render = e => ReactDOM.render(router.resolveRoute(e), document.querySelector('#app'))
 
-//initial render
 render(window.location.hash)
 
-//render on hash change
 window.onhashchange = render
--- src/lib/create-router.js
+++ src/lib/create-router.js
@@ -1,41 +1,29 @@
-import { pipe } from './helpers'
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
 
-//what about switch and exact matches?
+//what about switch and exact matches
 //maybe try enforcing type on all inputs to avoid errors
-//router container
+//how to create state
+//comment your code where necessary
 const createRouter = ({ routes, container, middlewares }) => {
 
-	//routes and middlewares must both be arrays
+	const handleHashChange = ({ newURL, oldURL }) => {
 
-	// extract new and old urls from hashchange event
-	const handleHashChange = ({ type, newURL, oldURL }) => {
-		if (!type || (type !== 'hashchange')) return console.log('oops')
-
-		const getRoute = url => routes.find(c => c.path === url.split('#')[1])
-
-		const routeAction = {
-			route: getRoute(newURL),
-			oldRoute: getRoute(oldURL)
-		}
-
-		// use async await here for the exit animation or if it is anync middleware
+		const data = { route: routes.find(c => c.path === newURL.split('#')[1]) || routes[0], oldURL }
 		
-		return middlewares ? pipe(...middlewares)(routeAction) : routeAction
+		return middlewares ? pipe(...middlewares)(data) : data
 	}
 
-	const resolveRoute = e => {
-		let routeAction
+	return {
+		resolveRoute: d => {
+			let data
 
-		e.type === 'hashchange'
-			? routeAction = handleHashChange(e)
-			: routeAction = { route: routes.find(c => c.path === e.slice(1)) || routes[0] }
+			d.type === 'hashchange'
+				? data = handleHashChange(d)
+				: data = { route: routes.find(c => c.path === d.slice(1)) || routes[0] }
 
-		const components = container ? container(routeAction.route.action()) : routeAction.route.action()
-
-		return components
+			return container ? container(data.route.action()) : data.route.action()
+		}
 	}
-
-	return { resolveRoute }
 }
 
 export default createRouter
