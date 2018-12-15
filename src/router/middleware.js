461c95c284167638840a233d9c4dd60c76b12ec6
David Woolf <davidjohnwoolf@gmail.com>
2018-12-15 10:05:14 -0800

    make middleware return new route object instead of mutating

--- src/router/middleware.js
+++ src/router/middleware.js
@@ -1,20 +1,15 @@
-//think of a better name
 export const addSlideDirectionToProps = (route) => {
 
 	if (!route.oldRoute) return route
 
 	const findPathIndex = (path) => route.routes.findIndex(c => c.path === path)
 
-	//maybe dont mutate this
-	route.props.slideForward = findPathIndex(route.newRoute.path) > findPathIndex(route.oldRoute.path)
+	const slideForward = findPathIndex(route.newRoute.path) > findPathIndex(route.oldRoute.path)
 
-	return route
+	return { ...route, props: { ...route.props, slideForward } }
 }
 
 export const addPathToProps = (route) => {
 
-	//maybe dont mutate this
-	route.props.path = route.newRoute.path
-
-	return route
+	return { ...route, props: { ...route.props, path: route.newRoute.path } }
 }
