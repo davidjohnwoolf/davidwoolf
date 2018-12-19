005b2d7c2c15b5542a12e91f46409dea27591bc3
David Woolf <davidjohnwoolf@gmail.com>
2018-12-18 16:02:58 -0800

    wrap contact links in p tags

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,15 +12,15 @@ const Contact = ({ slideForward }) => {
 					<h3>Contact</h3>
 					<div className="section-content">
 						<h4>Email</h4>
-						<a href="mailto:davidjohnwoolf@gmail.com">davidjohnwoolf@gmail.com</a>
+						<p><a href="mailto:davidjohnwoolf@gmail.com">davidjohnwoolf@gmail.com</a></p>
 						<h4>Phone</h4>
-						<a href="tel:8016339105">801.633.9105</a>
+						<p><a href="tel:8016339105">801.633.9105</a></p>
 						<h4>Location</h4>
-						<a href="https://goo.gl/maps/LW89sKg8YSK2" target="_blank">Salt Lake City, UT</a>
+						<p><a href="https://goo.gl/maps/LW89sKg8YSK2" target="_blank">Salt Lake City, UT</a></p>
 						<h4>Github</h4>
-						<a href="https://github.com/davidjohnwoolf" target="_blank">github.com/davidjohnwoolf</a>
+						<p><a href="https://github.com/davidjohnwoolf" target="_blank">github.com/davidjohnwoolf</a></p>
 						<h4>Linked In</h4>
-						<a href="https://linkedin.com/in/davidjohnwoolf" target="_blank">linkedin.com/in/davidjohnwoolf</a>
+						<p><a href="https://linkedin.com/in/davidjohnwoolf" target="_blank">linkedin.com/in/davidjohnwoolf</a></p>
 					</div>
 				</section>
 			</div>
