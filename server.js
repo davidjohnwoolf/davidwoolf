aa31eeabb097415f6c03f484f479fc3a7482f67c
David Woolf <davidjohnwoolf@gmail.com>
2019-01-19 12:51:20 -0800

    add 404 for node server

--- server.js
+++ server.js
@@ -16,12 +16,14 @@ const transporter = nodemailer.createTransport({
 
 app.use(logger('dev'))
 
+// static files
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }));
 
+// POST /contact
 app.post('/contact', (req, res) => {
 
 	const emailTemplate = ({ name, email, comment }) => {
@@ -47,6 +49,14 @@ app.post('/contact', (req, res) => {
 	}
 
 	return res.json({ status: 'failure', data: { message: 'Error: Request body missing name, email, or comment' } })
+})
+
+// handle 404
+app.use(function (req, res, next) {
+	res.status(404).send(
+		'<div style="text-align: center; margin: 2rem 0;"><h1>404 - Page Not Found</h1><p>The page '
+		+ req.path + ' does not exist</p><p><a href="/">Go to Site</a></p></div>'
+	)
 })
 
 app.listen(port, () => console.log(`listening on ${ port }...`))
