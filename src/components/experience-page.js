be2767feb92c9bc8e1e917f697883a5f5c80e857
David Woolf <davidjohnwoolf@gmail.com>
2019-01-22 14:46:08 -0800

    update page to implement box hoc

--- src/components/experience-page.js
+++ src/components/experience-page.js
@@ -1,63 +1,68 @@
 import React from 'react'
-import ContentBox from './content/content-box'
+import JobBox from './content/job-box'
 
 const ExperiencePage = ({ shouldSlideForward }) => {
 
 	return (
 		<div className={ `content ${ shouldSlideForward ? 'f-enter' : 'b-enter' }` } id="experience">
 			<div className="experience-sections sections">
-				<ContentBox
+				<JobBox
 					title="Full Stack Web Developer"
 					desc="Contracted to build a CRM for door to door sales. Started part time on the side and moved to full time in June of 2018."
 					tech="JavaScipt (ES6) with React/Redux, Node/Express, MongoDB, HTML5, CSS3/Sass, and Webpack"
 					hasRef={ true }
 					refTitle="Sergio Sanchez - Owner"
 					refPhone="8018670934"
-				>
-					<h4><a href="//kaizenrenewables.com/" target="_blank">Kaizen Solar</a> <span className="dates">Feb 2018 - Nov 2018</span></h4>
-				</ContentBox>
+					company="Kaizen Solar"
+					website="//kaizenrenewables.com/"
+					dates="Feb 2018 - Nov 2018"
+				/>
 
-				<ContentBox
+				<JobBox
 					title="Website Support II"
 					desc="Building custom website features and handling updates for priority client websites as well as customizing our eCommerce platform with vanilla JS."
 					tech="JavaScript, jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap"
 					hasRef={ true }
 					refTitle="Andrew Dobranski - Assistant Manager"
 					refPhone="9712804560"
-				>
-					<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">Dec 2016 - Jun 2018</span></h4>
-				</ContentBox>
+					company="Dealerspike"
+					website="https://www.dealerspike.com"
+					dates="Dec 2016 - Jun 2018"
+				/>
 
-				<ContentBox
+				<JobBox
 					title="Website Support"
 					desc="Working with clients to customize, update and maintain their websites on our inventory management platform."
 					tech="JavaScript, jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap"
 					hasRef={ true }
 					refTitle="Andrew Dobranski - Assistant Manager"
 					refPhone="9712804560"
-				>
-					<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">May 2016 - Dec 2016</span></h4>
-				</ContentBox>
+					company="Dealerspike"
+					website="https://www.dealerspike.com"
+					dates="May 2016 - Dec 2016"
+				/>
 
-				<ContentBox
+				<JobBox
 					title="Front End Web Developer"
 					desc="Building website templates for various charity fundraising events for use by organizations such as American Heart Association and Red Cross."
 					tech="HTML5, CSS3, Bootstrap, JavaScript"
 					hasRef={ true }
 					refTitle="Do Kim - Manager"
 					refPhone="9712804560"
-				>
-					<h4><a href="https://www.greatergiving.com/" target="_blank">Greater Giving</a> <span className="dates">Mar 2015 - May 2016</span></h4>
-				</ContentBox>
+					company="Greater Giving"
+					website="https://www.greatergiving.com/"
+					dates="Mar 2015 - May 2016"
+				/>
 
-				<ContentBox
+				<JobBox
 					title="Web Designer"
 					desc="Learning web development and occasionally building websites for friends, family and clients."
 					tech="JavaScript, HTML, CSS, jQuery, GIMP, Inkscape and limited PHP"
 					hasRef={ false }
-				>
-					<h4><a href="https://github.com/davidjohnwoolf/project_bold_rails" target="_blank">Freelance</a> <span className="dates">2008 - 2014</span></h4>
-				</ContentBox>
+					company="Freelance"
+					website="https://github.com/davidjohnwoolf/project_bold_rails"
+					dates="2008 - 2014"
+				/>
 			</div>
 		</div>
 	)
--- src/components/projects-page.js
+++ src/components/projects-page.js
@@ -1,55 +1,49 @@
 import React from 'react'
-import ContentBox from './content/content-box'
+import ProjectBox from './content/project-box'
 
 const ProjectsPage = ({ shouldSlideForward }) => {
 
 	return (
 		<div className={ `content ${ shouldSlideForward ? 'f-enter' : 'b-enter' }` } id="projects">
 			<div className="project-sections sections">
-				<ContentBox
+				<ProjectBox
 					title="Portfolio Site"
 					desc="A simple portfolio website to showcase my skills as a developer (you are looking at it now)."
 					tech="JavaScript with React, Node/Express, HTML5, CSS3/Sass, Webpack and deployed with Docker"
-					hasRef={ false }
-				>
-					<h4><a href="https://github.com/davidjohnwoolf/davidwoolf" target="_blank">Github</a></h4>
-				</ContentBox>
+					github="https://github.com/davidjohnwoolf/davidwoolf"
+					
+				/>
 
-				<ContentBox
+				<ProjectBox
 					title="Profession Quest"
 					desc="A collaborative capstone project for Portland Code School that pulls job postings from various different classifieds and combines them in one place."
 					tech="JavaScript with Backbone, Node with Express, MongoDB, HTML5, CSS3, tested with Mocha/Chai and deployed with Heroku"
-					hasRef={ false }
-				>
-					<h4><a href="https://professionquest.herokuapp.com/login" target="_blank">Website</a> <a href="https://github.com/professionQuest/professionQuest" target="_blank">Github</a></h4>
-				</ContentBox>
+					website="https://professionquest.herokuapp.com/login"
+					github="https://github.com/professionQuest/professionQuest"
+				/>
 
-				<ContentBox
+				<ProjectBox
 					title="Patchatech"
 					desc="A collaborative capstone project for DevPoint Labs that lets users view, upload, and comment on technical videos and tutorials."
 					tech="Ruby with Rails, JavaScript with jQuery, HTML5, CSS3, tested with RSpec and deployed with Heroku"
-					hasRef={ false }
-				>
-					<h4><a href="http://www.patchatech.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/patchatech" target="_blank">Github</a></h4>
-				</ContentBox>
+					website="http://www.patchatech.com"
+					github="https://github.com/davidjohnwoolf/patchatech"
+				/>
 
-				<ContentBox
+				<ProjectBox
 					title="Inform Feed Controller"
 					desc="A feed controller and organizer that pulls public feeds from the Facebook API and lets you view, organize and filter them anonymously."
 					tech="JavaScript with Mithril, Node/Express, HTML5, CSS3, Gulp and deployed with Heroku"
-					hasRef={ false }
-				>
-					<h4><a href="http://informfeedcontroller.herokuapp.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/inform" target="_blank">Github</a></h4>
-				</ContentBox>
+					website="http://informfeedcontroller.herokuapp.com"
+					github="https://github.com/davidjohnwoolf/inform"
+				/>
 
-				<ContentBox
+				<ProjectBox
 					title="Unit Builder"
 					desc="A custom trailer builder for the Dealerspike plaform that lets visitors customize their trailer and send it to the dealer."
 					tech="JavaScript with jQuery, JSON Data Structures, HTML5, CSS3/LESS"
-					hasRef={ false }
-				>
-					<h4><a href="https://www.nationwidetrailers.com/build-custom-pj-trailer-texas--unit-builder" target="_blank">Website</a></h4>
-				</ContentBox>
+					website="https://www.nationwidetrailers.com/build-custom-pj-trailer-texas--unit-builder"
+				/>
 			</div>
 		</div>
 	)
