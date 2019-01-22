b71e8add815c8fa7c3ab9d5278b5318fd48ee660
David Woolf <davidjohnwoolf@gmail.com>
2019-01-22 12:48:28 -0800

    update to use content box component

--- src/components/experience-page.js
+++ src/components/experience-page.js
@@ -1,94 +1,63 @@
 import React from 'react'
+import ContentBox from './content/content-box'
 
 const ExperiencePage = ({ shouldSlideForward }) => {
 
 	return (
 		<div className={ `content ${ shouldSlideForward ? 'f-enter' : 'b-enter' }` } id="experience">
 			<div className="experience-sections sections">
-				<section>
-					<h3>Full Stack Web Developer</h3>
-					<div className="section-content">
-						<h4><a href="//kaizenrenewables.com/" target="_blank">Kaizen Solar</a> <span className="dates">Feb 2018 - Nov 2018</span></h4>
-						<h4>Duty</h4>
-						<p>Contracted to build a CRM for door to door sales. Started part time on the side and moved to full time in June of 2018.</p>
-						<h4>Technology</h4>
-						<p>JavaScipt (ES6) with React/Redux, Node/Express, MongoDB, HTML5, CSS3/Sass, and Webpack</p>
-						<div>
-							<h4>Reference</h4>
-							<p>
-								Sergio Sanchez - Owner
-								<br />
-								<a href="tel:8018670934">801.867.0934</a>
-							</p>
-						</div>
-					</div>
-				</section>
-				<section>
-					<h3>Website Support II</h3>
-					<div className="section-content">
-						<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">Dec 2016 - Jun 2018</span></h4>
-						<h4>Duty</h4>
-						<p>Building custom website features and handling updates for priority client websites as well as customizing our eCommerce platform with vanilla JS.</p>
-						<h4>Technology</h4>
-						<p>JavaScript, jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap</p>
-						<div>
-							<h4>Reference</h4>
-							<p>
-								Andrew Dobranski - Assistant Manager
-								<br />
-								<a href="tel:9712804560">971.280.4560</a>
-							</p>
-						</div>
-					</div>
-				</section>
-				<section>
-					<h3>Website Support</h3>
-					<div className="section-content">
-						<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">May 2016 - Dec 2016</span></h4>
-						<h4>Duty</h4>
-						<p>Working with clients to customize, update and maintain their websites on our inventory management platform.</p>
-						<h4>Technology</h4>
-						<p>JavaScript with jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap</p>
-						<div>
-							<h4>Reference</h4>
-							<p>
-								Andrew Dobranski - Assistant Manager
-								<br />
-								<a href="tel:9712804560">971.280.4560</a>
-							</p>
-						</div>
-					</div>
-				</section>
-				<section>
-					<h3>Front End Web Developer</h3>
-					<div className="section-content">
-						<h4><a href="https://www.greatergiving.com/" target="_blank">Greater Giving</a> <span className="dates">Mar 2015 - May 2016</span></h4>
-						<h4>Duty</h4>
-						<p>Building website templates for various charity fundraising events for use by organizations such as American Heart Association and Red Cross.</p>
-						<h4>Technology</h4>
-						<p>JavaScript, HTML5, CSS3, and Bootstrap</p>
-						<div>
-							<h4>Reference</h4>
-							<p>
-								Do - Manager
-								<br />
-								<a href="tel:9712804560">971.280.4560</a>
-							</p>
-						</div>
-					</div>
-				</section>
-				<section>
-					<h3>Front End Web Developer</h3>
-					<div className="section-content">
-						<h4><a href="https://github.com/davidjohnwoolf/project_bold_rails" target="_blank">Freelance</a> <span className="dates">2008 - 2014</span></h4>
-						<h4>Duty</h4>
-						<p>Learning web development and occasionally building websites for friends, family and clients.</p>
-						<div>
-							<h4>Technology</h4>
-							<p>JavaScript, HTML, CSS, PHP, and jQuery</p>
-						</div>
-					</div>
-				</section>
+				<ContentBox
+					title="Full Stack Web Developer"
+					desc="Contracted to build a CRM for door to door sales. Started part time on the side and moved to full time in June of 2018."
+					tech="JavaScipt (ES6) with React/Redux, Node/Express, MongoDB, HTML5, CSS3/Sass, and Webpack"
+					hasRef={ true }
+					refTitle="Sergio Sanchez - Owner"
+					refPhone="8018670934"
+				>
+					<h4><a href="//kaizenrenewables.com/" target="_blank">Kaizen Solar</a> <span className="dates">Feb 2018 - Nov 2018</span></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Website Support II"
+					desc="Building custom website features and handling updates for priority client websites as well as customizing our eCommerce platform with vanilla JS."
+					tech="JavaScript, jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap"
+					hasRef={ true }
+					refTitle="Andrew Dobranski - Assistant Manager"
+					refPhone="9712804560"
+				>
+					<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">Dec 2016 - Jun 2018</span></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Website Support"
+					desc="Working with clients to customize, update and maintain their websites on our inventory management platform."
+					tech="JavaScript, jQuery, ASP, HTML5, CSS3/LESS, and Bootstrap"
+					hasRef={ true }
+					refTitle="Andrew Dobranski - Assistant Manager"
+					refPhone="9712804560"
+				>
+					<h4><a href="https://www.dealerspike.com" target="_blank">Dealerspike</a> <span className="dates">May 2016 - Dec 2016</span></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Front End Web Developer"
+					desc="Building website templates for various charity fundraising events for use by organizations such as American Heart Association and Red Cross."
+					tech="HTML5, CSS3, Bootstrap, JavaScript"
+					hasRef={ true }
+					refTitle="Do Kim - Manager"
+					refPhone="9712804560"
+				>
+					<h4><a href="https://www.greatergiving.com/" target="_blank">Greater Giving</a> <span className="dates">Mar 2015 - May 2016</span></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Web Designer"
+					desc="Learning web development and occasionally building websites for friends, family and clients."
+					tech="JavaScript, HTML, CSS, jQuery, GIMP, Inkscape and limited PHP"
+					hasRef={ false }
+				>
+					<h4><a href="https://github.com/davidjohnwoolf/project_bold_rails" target="_blank">Freelance</a> <span className="dates">2008 - 2014</span></h4>
+				</ContentBox>
 			</div>
 		</div>
 	)
--- src/components/projects-page.js
+++ src/components/projects-page.js
@@ -1,76 +1,56 @@
 import React from 'react'
+import ContentBox from './content/content-box'
 
 const ProjectsPage = ({ shouldSlideForward }) => {
 
 	return (
 		<div className={ `content ${ shouldSlideForward ? 'f-enter' : 'b-enter' }` } id="projects">
 			<div className="project-sections sections">
-				<section>
-					<h3>Portfolio Site</h3>
-					<div className="section-content">
-						<h4><a href="https://github.com/davidjohnwoolf/davidwoolf" target="_blank">Github</a></h4>
+				<ContentBox
+					title="Portfolio Site"
+					desc="A simple portfolio website to showcase my skills as a developer (you are looking at it now)."
+					tech="JavaScript with React, Node/Express, HTML5, CSS3/Sass, Webpack and deployed with Docker"
+					hasRef={ false }
+				>
+					<h4><a href="https://github.com/davidjohnwoolf/davidwoolf" target="_blank">Github</a></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Profession Quest"
+					desc="A collaborative capstone project for Portland Code School that pulls job postings from various different classifieds and combines them in one place."
+					tech="JavaScript with Backbone, Node with Express, MongoDB, HTML5, CSS3, tested with Mocha/Chai and deployed with Heroku"
+					hasRef={ false }
+				>
+					<h4><a href="https://professionquest.herokuapp.com/login" target="_blank">Website</a> <a href="https://github.com/professionQuest/professionQuest" target="_blank">Github</a></h4>
 						<h4>Description</h4>
-						<p>A simple portfolio website to showcase my skills as a developer (you are looking at it now).</p>
-						
-						<h4>Technology</h4>
-						<p>JavaScript with React, Node/Express, HTML5, CSS3/Sass, Webpack and deployed with Docker</p>
-					</div>
-				</section>
-				<section>
-					<h3>Inform</h3>
-					<div className="section-content">
-						<h4><a href="http://informfeedcontroller.herokuapp.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/inform" target="_blank">Github</a></h4>
-						<h4>Description</h4>
-						<p>A feed controller and organizer that pulls public feeds from the Facebook API and lets you view, organize and filter them anonymously.</p>
-						
-						<h4>Technology</h4>
-						<p>JavaScript with Mithril, Node/Express, HTML5, CSS3, Gulp and deployed with Heroku</p>
-					</div>
-				</section>
-				<section>
-					<h3>Patchatech</h3>
-					<div className="section-content">
-						<h4><a href="http://www.patchatech.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/inform" target="_blank">Github</a></h4>
-						<h4>Description</h4>
-						<p>A collaborative capstone project for DevPoint Labs that lets users view, upload, and comment on technical videos and tutorials.</p>
-						
-						<h4>Technology</h4>
-						<p>Ruby with Rails, JavaScript with jQuery, HTML5, CSS3/Sass, tested with RSpec and deployed with Heroku</p>
-					</div>
-				</section>
-				<section>
-					<h3>Profession Quest</h3>
-					<div className="section-content">
-						<h4><a href="https://professionquest.herokuapp.com/login" target="_blank">Website</a> <a href="https://github.com/professionQuest/professionQuest" target="_blank">Github</a></h4>
-						<h4>Description</h4>
-						<p>A collaborative capstone project for Portland Code School that pulls job postings from various different classifieds and combines them in one place.</p>
-						
-						<h4>Technology</h4>
-						<p>JavaScript with backbone, Node with Express, HTML5, CSS3, tested with Mocha/Chai and deployed with Heroku</p>
-					</div>
-				</section>
-				{ /*<section>
-					<h3>Something Else</h3>
-					<div className="section-content">
-						<h4><a href="https://ahautah.ejoinme.org/MyEvents/20172018UtahHeartBall/tabid/872317/Default.aspx" target="_blank">Website</a></h4>
-						<h5>Description</h5>
-						<p>A template I built for the American Heart Associations Heart Ball Fundraiser (site linked above is an implementation of the template).</p>
-						
-						<h5>Technology</h5>
-						<p>HTML5, CSS3, JavaScript</p>
-					</div>
-				</section> */ }
-				<section>
-					<h3>Unit Builder</h3>
-					<div className="section-content">
-						<h4><a href="https://www.nationwidetrailers.com/build-custom-pj-trailer-texas--unit-builder" target="_blank">Website</a></h4>
-						<h4>Description</h4>
-						<p>A custom trailer builder for the Dealerspike plaform that lets visitors customize their trailer and send it to the dealer.</p>
-						
-						<h4>Technology</h4>
-						<p>HTML5, CSS3/LESS, JavaScript with jQuery</p>
-					</div>
-				</section>
+				</ContentBox>
+
+				<ContentBox
+					title="Patchatech"
+					desc="A collaborative capstone project for DevPoint Labs that lets users view, upload, and comment on technical videos and tutorials."
+					tech="Ruby with Rails, JavaScript with jQuery, HTML5, CSS3, tested with RSpec and deployed with Heroku"
+					hasRef={ false }
+				>
+					<h4><a href="http://www.patchatech.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/patchatech" target="_blank">Github</a></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Inform Feed Controller"
+					desc="A feed controller and organizer that pulls public feeds from the Facebook API and lets you view, organize and filter them anonymously."
+					tech="JavaScript with Mithril, Node/Express, HTML5, CSS3, Gulp and deployed with Heroku"
+					hasRef={ false }
+				>
+					<h4><a href="http://informfeedcontroller.herokuapp.com" target="_blank">Website</a> <a href="https://github.com/davidjohnwoolf/inform" target="_blank">Github</a></h4>
+				</ContentBox>
+
+				<ContentBox
+					title="Unit Builder"
+					desc="A custom trailer builder for the Dealerspike plaform that lets visitors customize their trailer and send it to the dealer."
+					tech="JavaScript with jQuery, JSON Data Structures, HTML5, CSS3/LESS"
+					hasRef={ false }
+				>
+					<h4><a href="https://www.nationwidetrailers.com/build-custom-pj-trailer-texas--unit-builder" target="_blank">Website</a></h4>
+				</ContentBox>
 			</div>
 		</div>
 	)
